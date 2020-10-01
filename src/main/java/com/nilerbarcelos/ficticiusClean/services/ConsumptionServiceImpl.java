dc5cbf1c763bbc1794082eaf844e046fbfa1f5e0
Niler Barcelos <nilerbarcelos@gmail.com>
2020-10-01 10:45:48 -0300

    Update ConsumptionServiceImpl.java

--- src/main/java/com/nilerbarcelos/ficticiusClean/services/ConsumptionServiceImpl.java
+++ src/main/java/com/nilerbarcelos/ficticiusClean/services/ConsumptionServiceImpl.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class ConsumptionServiceImpl implements ConsumptionService {
     @Override
-    public float computeTotalPrice(float fuelPrice, float consumedFuel) {
+    public BigDecimal computeTotalPrice(BigDecimal fuelPrice, BigDecimal consumedFuel) {
         return fuelPrice * consumedFuel;
     }
 
     @Override
-    public float computeUsage(float distance, float averageConsumption) {
+    public BigDecimal computeUsage(BigDecimal distance, BigDecimal averageConsumption) {
         return distance / averageConsumption;
     }
 }
